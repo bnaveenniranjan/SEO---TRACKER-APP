0f91690c26fbd2bc49795e0b36a3e15cabe88aff
bnaveenniranjan <112605495+bnaveenniranjan@users.noreply.github.com>
2026-06-04 15:35:25 +0530

    front end UI and backend connected

---
+++ server/controllers/authControllers.js
@@ -0,0 +1,64 @@
+import User from "../models/user.js";
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken";
+
+const generateToken =(id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET,{EXP}{expiresIn : "30d"})
+}
+
+//Register user
+
+export const register = async (req,res) => {
+    try{
+        const {name,email,password} = req.body;
+
+        if(!name,!email,!password) return res.status(400).json({success : false,message:"all fields ae required "});
+        
+        //Checks if user exists
+        const existingUser = await User.findOne({email})
+        if(existingUser) return res.status(400).json({success : false,message:"User already exists"});
+
+        //Hash password
+        const hashedPassword = await bcrypt.hash(password,await bcrypt.genSalt(10))
+
+        // Create user
+        const user = await User.create({name,email,password:hashedPassword})
+        
+        const token = generateToken(user._id);
+
+        res.status(201).json({})
+
+    }catch(error){
+        console.error("Register error:",error.message)
+        res.status(500).json({success:false,message:"server error"})
+
+    }
+}
+//login user
+
+export const login = async (req,res) => {
+    try{
+        const {name,email,password} = req.body;
+
+        if(!name,!email,!password) return res.status(400).json({success : false,message:"all fields ae required "});
+        
+        //Checks if user exists
+        const existingUser = await User.findOne({email})
+        if(existingUser) return res.status(400).json({success : false,message:"User already exists"});
+
+        //Hash password
+        const hashedPassword = await bcrypt.hash(password,await bcrypt.genSalt(10))
+
+        // Create user
+        const user = await User.create({name,email,password:hashedPassword})
+        
+        const token = generateToken(user._id);
+
+        res.status(201).json({})
+
+    }catch(error){
+        console.error("Register error:",error.message)
+        res.status(500).json({success:false,message:"server error"})
+
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
+import  dotenv  from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+
+app.get("/",(req,res) =>{
+    res.send("API Working");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
